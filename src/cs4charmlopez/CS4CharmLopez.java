bbcfdb1330d30494bd54b8ffe33a735fd77e79bb
lmclopez <112681529+lmclopez@users.noreply.github.com>
2023-08-29 22:15:10 +0800

    Update CS4CharmLopez.java

--- src/cs4charmlopez/CS4CharmLopez.java
+++ src/cs4charmlopez/CS4CharmLopez.java
@@ -16,7 +16,28 @@ public class CS4CharmLopez {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String char1name  = "Xiling";
+        String char1name  = "Hu Tao";
+        int char1age = 20;
+        String char1affil = "Wangsheng Funeral Parlor";
+
+        String char2name = "Collei";
+        int char2age = 16;
+        String char2affil = "Gandhara Ville";
+
+        int combinedage = char1age + char2age;
+        int ageratio1to2 = char1age / char2age;
+        bool iscollei = (char2affil==Akademiya); 
+
+        System.out.printIn("Character 1\n Name: " + char1name + "/n Age: " + char1age + "/n Affiliation: " + char1affil + "/n /n" );
+
+        System.out.printIn("Character 2\n Name: " + char2name + "/n Age: " + char2age + "/n Affiliation: " + char2affil + "/n /n" );
+
+        //The combined age of the two characters 
+        //The age ratio of character 1 to character 2 
+        //Is Collei a member of the Akademiya?
+        
+
+        
         
     } 
     
